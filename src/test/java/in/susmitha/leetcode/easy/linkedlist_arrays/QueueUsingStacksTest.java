1d10f9b6dbd6399ed0e9ee870d35889ac8cad77c
susmitha-coder <79389510+susmitha-coder@users.noreply.github.com>
2021-06-05 22:46:36 +0530

    Update QueueUsingStacksTest.java

--- src/test/java/in/susmitha/leetcode/easy/linkedlist_arrays/QueueUsingStacksTest.java
+++ src/test/java/in/susmitha/leetcode/easy/linkedlist_arrays/QueueUsingStacksTest.java
@@ -3,3 +3,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
+
+class QueueUsingStacksTest {
+
+    @Test
+    public void testCorrectOperation(){
+        QueueUsingStacks stack = new QueueUsingStacks();
+//        int result = qu.push(1);
+//        Assertions.assertEquals(1,result);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Assertions.assertEquals(stack.empty(),false);
+        Assertions.assertEquals(stack.top(),3);
+        Assertions.assertEquals(stack.pop(),3);
+        Assertions.assertEquals(stack.top(),2);
+        Assertions.assertEquals(stack.pop(),2);
+        Assertions.assertEquals(stack.top(),1);
+        Assertions.assertEquals(stack.pop(),1);
+        Assertions.assertEquals(stack.top(),-1);
+        Assertions.assertEquals(stack.empty(),true);
+    }
+
+
+}
