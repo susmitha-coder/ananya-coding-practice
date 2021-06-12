b36d17455e2b2dc46a13b14853b29e28223bd41c
susmitha-coder <79389510+susmitha-coder@users.noreply.github.com>
2021-06-12 23:59:53 +0530

    Create AddDigitsTest.java

---
+++ src/test/java/in/susmitha/leetcode/easy/linkedlist_arrays/AddDigitsTest.java
@@ -0,0 +1,15 @@
+package in.susmitha.leetcode.easy.linkedlist_arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class AddDigitsTest {
+    @Test
+    public void testCaseLegalInput() {
+        AddDigits pn = new AddDigits();
+        Assertions.assertEquals(pn.addDigits(23), 5);
+
+    }
+    }
