fd32f9e5aea72126277ed3b378d176ddb53bb0f5
susmitha-coder <79389510+susmitha-coder@users.noreply.github.com>
2021-06-21 19:36:11 +0530

    Update LinkedList.java

--- src/main/java/in/susmitha/design/linkedlist/model/LinkedList.java
+++ src/main/java/in/susmitha/design/linkedlist/model/LinkedList.java
@@ -26,6 +26,7 @@ public class LinkedList {
 
     }
 
+
     public boolean deleteFront(){
         if(head!= null) {
             head = head.getNext();
@@ -67,7 +68,110 @@ public class LinkedList {
 
     }
 
+    public void insertAtGivenPosition(int val,int pos){
+        LinkedListNode node = new LinkedListNode(val);
+        if(pos<1){
+            System.out.println("Invalid position");
+        }
+        if(head == null && pos != 1){
+            return;
+        }
+        if(head == null && pos==1){
+            head = node;
+        }
+        if(head != null && pos == 1){
 
+            node.setNext(head);
+            head=node;
+            return;
+        }
+
+        LinkedListNode temp = head;
+        LinkedListNode prev = null;
+        int i=1;
+        while(i<pos){
+            prev = temp;
+            temp = temp.getNext();
+            if(temp == null)
+                break;
+            i++;
+        }
+        node.setNext(temp);
+        prev.setNext(node);
+    }
+
+    public int getSize(){
+        LinkedListNode temp = head;
+        int cnt=0;
+        if(head==null)
+            return 0;
+        else{
+
+            while (temp.getNext() != null) {
+                temp= temp.getNext();
+                cnt++;
+            }
+
+
+        }
+        return cnt+1;
+    }
+
+
+    public void deleteAtGivenPosition(int pos){
+
+        if(head == null){
+            System.out.println("The list is empty");
+            return;
+        }
+        if(pos<1){
+            System.out.println("The position should be greater than or equal to 1");
+            return;
+        }
+        LinkedListNode temp = head;
+        if(pos == 1){
+            head = temp.getNext();
+            return;
+        }
+
+        for(int i=1; temp!=null && i < pos-1 ;i++){
+            temp = temp.getNext();
+        }
+        if (temp == null || temp.getNext() == null) {
+            return;
+        }
+
+        LinkedListNode next = temp.getNext().getNext();
+        temp.setNext(next);
+    }
+    public boolean check(int val){
+        if(head == null){
+            System.out.println("The given list has no nodes");
+            return false;
+        }
+        else{
+            LinkedListNode temp = head;
+            while(temp!=null){
+                if(temp.getValue() == val)
+                    return true;
+                temp = temp.getNext();
+            }
+            return false;
+        }
+    }
+    public  void reverse(){
+        LinkedListNode prev = null;
+        LinkedListNode current = head;
+        LinkedListNode next = null;
+        while(current != null){
+            next = current.getNext();
+            current.setNext(prev);
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+    }
     public void print() {
         System.out.print("head -> ");
 
