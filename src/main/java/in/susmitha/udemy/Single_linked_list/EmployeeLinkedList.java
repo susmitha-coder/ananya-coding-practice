9fee44e9ae7653e94430771a66613198766034fb
susmitha-coder <79389510+susmitha-coder@users.noreply.github.com>
2021-07-25 09:28:18 +0530

    Single linked list

---
+++ src/main/java/in/susmitha/udemy/Single_linked_list/EmployeeLinkedList.java
@@ -0,0 +1,72 @@
+package in.susmitha.udemy.Single_linked_list;
+
+public class EmployeeLinkedList {
+        private EmployeeNode head;
+        private int size;
+
+        public void addToFront(Employee employee) {
+            EmployeeNode node = new EmployeeNode(employee);
+            node.setNext(head);
+            head = node;
+            size++;
+        }
+
+        public EmployeeNode removeFromFront() {
+            if (isEmpty()) {
+                return null;
+            }
+
+            EmployeeNode removedNode = head;
+            head = head.getNext();
+            size--;
+            removedNode.setNext(null);
+            return removedNode;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void printList() {
+            EmployeeNode current = head;
+            System.out.print("HEAD -> ");
+            while (current != null) {
+                System.out.print(current);
+                System.out.print(" -> ");
+                current = current.getNext();
+            }
+            System.out.println("null");
+        }
+
+
+
+        public Iterator iterator(){
+
+            return new Iterator(head);
+        }
+
+        public class Iterator{
+            private EmployeeNode ptr;
+
+            public Iterator(EmployeeNode head){
+
+                this.ptr = head;
+            }
+
+            public boolean hasNext(){
+
+                return ptr != null;
+            }
+
+            public int next(){
+                int result = ptr == null?-1:ptr.getEmployee().getId();
+                ptr = ptr == null?ptr:ptr.getNext();
+                return result;
+            }
+        }
+    }
+
